387cd12d8b32f5d1a42441b2465f157a129e729f
steklyanov <steklyanov@gmail.com>
2019-04-21 18:36:07 +0300

    some changes

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -5,7 +5,7 @@ if ($argc > 1)
     $arr = array();
     for ($i = 1; $i < $argc; $i++) {
         $string = preg_replace('/ +/', ' ', $argv[$i]);
-        $arr = array_push($string, explode(' ', ));
+        $arr = array_push($string, explode(' ', $st));
     }
     sort($arr);
     foreach ($arr as $s) {
