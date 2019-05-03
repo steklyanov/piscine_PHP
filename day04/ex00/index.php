2b463895a04c095efab922ee30d278f88a5cb54f
Maron martell Mraz <steklyanov@gmail.com>
2019-05-03 18:15:30 +0300

    qqq

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,14 +1,19 @@
 <?php
-session_start();
-if ($_GET["login"])
+    session_start();
+
+    if ($_GET["login"] and $_GET["passwd"] and $_GET["submit"] === "OK")
+    {
+        $_SESSION["login"] = $_GET["login"];
+        $_SESSION["passwd"] =$_GET["passwd"];
+    }
 ?>
 
 <html><body>
-<form action="index.php" method="GET">
-    Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
-    <br />
-    Mot de passe: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
-    <br />
-    <input type="submit" name="submit" value="OK"/>
-</form>
+    <form action="index.php" method="GET">
+        login: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
+        <br />
+        passwd: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
+        <br />
+        <input type="submit" name="submit" value="OK"/>
+    </form>
 </body></html>
