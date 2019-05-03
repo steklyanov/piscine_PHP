8d2130377f733a6d8225265ad152a8fce1426f31
Maron martell Mraz <steklyanov@gmail.com>
2019-05-03 17:33:26 +0300

    fix bug

--- day02/ex00/another_world.php
+++ day02/ex00/another_world.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
 <?php
 if ($argc > 1) {
-    print (implode(' ', preg_split("/\s+/", $argv[1]))."\n");
+    print (implode(' ', preg_split("/[ \t]+/", $argv[1]))."\n");
 }
--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,1 +1,14 @@
 <?php
+session_start();
+if ($_GET["login"])
+?>
+
+<html><body>
+<form action="index.php" method="GET">
+    Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
+    <br />
+    Mot de passe: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
+    <br />
+    <input type="submit" name="submit" value="OK"/>
+</form>
+</body></html>
