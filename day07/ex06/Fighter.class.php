30efb9caec04bfcd06acf5fc074cc419424b028e
Maron martell Mraz <steklyanov@gmail.com>
2019-05-09 21:02:09 +0300

    finish day

--- day07/ex06/Fighter.class.php
+++ day07/ex06/Fighter.class.php
@@ -1,1 +1,14 @@
 <?php
+
+class Fighter
+{
+    public $f_name;
+    public function __construct($name)
+    {
+        $this->f_name = $name;
+    }
+    public function getName()
+    {
+        return $this->f_name;
+    }
+}
--- day07/ex06/UnholyFactory.class.php
+++ day07/ex06/UnholyFactory.class.php
@@ -1,1 +1,28 @@
 <?php
+class UnholyFactory
+{
+    private $army = array();
+    function absorb($obj)
+    {
+        if (!in_array($obj, $this->army))
+        {
+            $this->army[] = $obj;
+            print "(Factory absorbed a fighter of type ".$obj->f_name.")".PHP_EOL;
+        }
+        else
+            print "(Factory already absorbed a fighter of type ".$obj->f_name.")".PHP_EOL;
+
+    }
+    function fabricate($name)
+    {
+        foreach ($this->army as $fighter) {
+            if ($fighter->f_name == $name)
+            {
+                print "(Factory fabricate a fighter of type ".$fighter->f_name.")".PHP_EOL;
+                return $fighter;
+            }
+
+        }
+        return null;
+    }
+}
